59f7ae15765dd2fd402f7a12bcdbbef86268cbd5
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-24 17:54:47 +0100

    added footer with Github link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://https://adoring-snyder-02ab2d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Julia Planchon
+        </a>{" "}
+        and it's open source at{" "}
+        <a
+          href="https://https://github.com/jujuPlanchon/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Julia's Github
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
