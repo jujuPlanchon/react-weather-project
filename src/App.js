d97f869e4e138d05aad3d76d86fb30d59e7cd47c
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-25 16:10:03 +0100

    fixed cityDefault

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Cointrin" />
         <footer>
           Coded by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -69,8 +69,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "89edb83e45300d77a9a1a3bef122681f";
-    let city = "Genève";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
