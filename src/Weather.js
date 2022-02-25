63d4a3e86eb6aba6fbc1946eb0166cef194e312e
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-25 15:06:42 +0100

    styled all the component Weather

--- src/Weather.js
+++ src/Weather.js
@@ -10,13 +10,17 @@ export default function Weather() {
         <li>06:00</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="Sunny"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+              className="float-left"
+            />
+            <span className="temperature float-left">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
@@ -26,7 +30,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-
       <form>
         <div className="row">
           <div className="col-9">
@@ -34,10 +37,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
