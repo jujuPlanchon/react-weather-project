76fb45ab795f0dc627851feaf334e6ca2a59f2a9
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-25 13:01:42 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        Coded by{" "}
-        <a
-          href="https://adoring-snyder-02ab2d.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julia Planchon
-        </a>{" "}
-        and it's open source at{" "}
-        <a
-          href="https://github.com/jujuPlanchon/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julia's Github
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://adoring-snyder-02ab2d.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julia Planchon
+          </a>{" "}
+          and it's open source at{" "}
+          <a
+            href="https://github.com/jujuPlanchon/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julia's Github
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
