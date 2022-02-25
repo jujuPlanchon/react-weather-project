e74e7759574f93f3a6c82034d65fcd4e363ad8ca
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-25 17:10:57 +0100

    created new component for Hour

---
+++ src/FormatedHour.js
@@ -0,0 +1,11 @@
+export default function FormatedHour(props) {
+  let hours = props.hour.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.hour.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
